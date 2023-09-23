a039a62fed5670f4a11c40bac530f80df8628ad3
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-09-23 15:15:05 +1000

    Bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+const arr = [9, 3, 2, 5, 6, 4];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(arr));
--- selectionSort.js
+++ selectionSort.js
@@ -1,7 +1,4 @@
-const arr = [
-  0, 3, 2, 5, 6, 8, 1, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3,
-  32,
-];
+const arr = [9, 3, 2, 5, 6];
 
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
